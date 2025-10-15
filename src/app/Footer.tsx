16ec099e192cd5513496303b16f22cb43bfce6a1
leonegithub <webmaster@leone.it>
2025-10-15 15:53:32 +0200

    fix: footer style

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -5,12 +5,12 @@ export default function Footer() {
     return (
         <footer className="bg-gray-200 blue">
             {/* Widgets */}
-            <section className="py-4 md:py-5 xl:py-6">
+            <section className="footer-first-section md:py-5 xl:py-6">
                 <div className="container mx-auto px-4">
-                    <div className="grid grid-cols-6 md:grid-cols-12 gap-3">
-                        <div className="col-span-3 md:col-span-4">
+                    <div className="grid grid-cols-2 md:grid-cols-4 gap-3">
+                        <div className="col-span-1">
                             <div className="link-wrapper">
-                                <div className="mb-1 font-bold">Services</div>
+                                <div className="mb-1 font-bold">Servizi</div>
                                 <ul className="m-0 list-none pl-0">
                                     <li className="mb-1">
                                         <Link href={"/services/downloads"}>Downloads</Link>
@@ -18,33 +18,72 @@ export default function Footer() {
                                     <li className="mb-1">
                                         <Link href={`/services/newlist`}>Newlist</Link>
                                     </li>
-                                    {/*  <li className="mb-1">
-                    <Link href={`/services/customer-support`}>
-                      Customer support
-                    </Link>
-                  </li> */}
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col-span-3 md:col-span-4">
-                            <div className="link-wrapper">
-                                <div className="mb-1 font-bold">Scientific publications</div>
-                                <ul className="m-0 list-none pl-0">
-                                    <Link className="mb-1" href={`/products/publications`}>
-                                        Orthodontics
-                                    </Link>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col-span-3 md:col-span-4">
-                            <div className="link-wrapper">
-                                <div className="mb-1 font-bold">Quality</div>
-                                <ul className="m-0 list-none pl-0">
                                     <li className="mb-1">
-                                        <Link href={`/quality/safety-sheets`}>Safety sheets</Link>
+                                        <Link href={`/services/customer-support`}>
+                                            Assistenza clienti
+                                        </Link>
                                     </li>
                                     <li className="mb-1">
-                                        <Link href={`/quality/quality-system`}>Quality system</Link>
+                                        <Link href={`/services/customer-support`}>
+                                            Area Riservata
+                                        </Link>
+                                    </li>
+                                    <li className="mb-1">
+                                        <Link href={`/services/customer-support`}>
+                                            Whistleblowing
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-span-1">
+                            <div className="link-wrapper">
+                                <div className="mb-1 font-bold">Pubblicazioni scientifiche</div>
+                                <ul className="m-0 list-none pl-0">
+                                    <li className="mb-1">
+                                        <Link href={`/products/publications`}>
+                                            Ortodonzia
+                                        </Link>
+                                    </li>
+                                    <li className="mb-1">
+                                        <Link href={`/products/publications`}>
+                                            Implantologia
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-span-1">
+                            <div className="link-wrapper">
+                                <div className="mb-1 font-bold">Qualità</div>
+                                <ul className="m-0 list-none pl-0">
+                                    <li className="mb-1">
+                                        <Link href={`/quality/quality-system`}>Sistema di Qualità</Link>
+                                    </li>
+                                    <li className="mb-1">
+                                        <Link href={`/quality/safety-sheets`}>Schede di Sicurezza</Link>
+                                    </li>
+                                    <li className="mb-1">
+                                        <Link href={`/products/publications`}>
+                                            Istruzioni
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-span-1">
+                            <div className="link-wrapper">
+                                <div className="mb-1 font-bold">Distributori</div>
+                                <ul className="m-0 list-none pl-0">
+                                    <li className="mb-1">
+                                        <Link href={`/distributors/worldwide`}>
+                                            Ortodonzia
+                                        </Link>
+                                    </li>
+                                    <li className="mb-1">
+                                        <Link href={`/products/publications`}>
+                                            Implantologia
+                                        </Link>
                                     </li>
                                 </ul>
                             </div>
@@ -55,26 +94,16 @@ export default function Footer() {
             {/* Social Media */}
             <div className="pb-4">
                 <div className="container mx-auto px-4">
-                    <div className="grid grid-cols-6 md:grid-cols-12 gap-3 items-start sm:items-center">
-                        <div className="col-span-3 md:col-span-4">
-                            <div className="link-wrapper">
-                                <strong>LeoneAmerica Dental Products, Inc</strong>
-                                <br />
-                                1400 Graves Ave. Unit A | Oxnard, CA 93030
-                                <br />
-                                info@leoneamerica.com
-                                <br />
-                            </div>
+                    <div className="grid grid-cols-2 md:grid-cols-4 gap-3">
+                        <div className="col-span-3 md:col-span-3">
+                            <strong>Leone S.p.A.</strong> <br/>
+                            Via P. a Quaracchi, 50 - 50019 Sesto Fiorentino, Firenze
+                            P. IVA 01686960483 Uff. Reg. Imprese Firenze n. 01686960483
+                            <em>Cap. soc. Euro 1.200.000,00 int. vers.</em>
                         </div>
-                        <div className="col-span-3 md:col-span-4 flex">
-                            <em className="max-w-80 pt-4">
-                                Sole distributor of Leone SpA orthodontic products in the USA,
-                                Canada, and Puerto Rico
-                            </em>
-                        </div>
-                        <div className="col-span-3 md:col-span-4 flex">
+                        <div className="col-span-1 md:col-span-1 md:col-start-4">
                             <div className="social-media-wrapper text-4xl">
-                                <ul className="m-0 socials-list list-none pl-0 flex justify-start sm:justify-end gap-3 xl:gap-4">
+                                <ul className="m-0 socials-list list-none pl-0 flex gap-8 xl:gap-4">
                                     <li>
                                         <Link
                                             className="hover:opacity-75"
